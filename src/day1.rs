e018a0a852e2421afd911ec4326381c0fb5bd7f8
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-12-01 06:47:16 +0100

    Day 1

---
+++ src/day1.rs
@@ -0,0 +1,32 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use itertools::Itertools;
+
+pub const PARTS: [fn(); 2] = [part1, part2];
+
+pub fn part1() {
+    let ans = BufReader::new(File::open("input/day1/input").unwrap())
+        .lines()
+        .map(|l| l.unwrap().parse::<u64>().unwrap())
+        .tuple_windows()
+        .filter(|(a, b)| a < b)
+        .count();
+
+    println!("{}", ans);
+}
+
+pub fn part2() {
+    let ans = BufReader::new(File::open("input/day1/input").unwrap())
+        .lines()
+        .map(|l| l.unwrap().parse::<u64>().unwrap())
+        .tuple_windows()
+        .map(|(a, b, c)| a + b + c)
+        .tuple_windows()
+        .filter(|(a, b)| a < b)
+        .count();
+
+    println!("{}", ans);
+}
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+mod day1;
+
+fn main() {
+    let funcs = [
+        day1::PARTS,
+    ];
+    let mut args = std::env::args();
+    args.next();
+    if let Some(x) = args.next() {
+        if let Ok(x) = x.parse::<usize>() {
+            if let Some(y) = args.next() {
+                if let Ok(y) = y.parse::<usize>() {
+                    if let Some(x) = funcs.get(x - 1) {
+                        if let Some(x) = x.get(y - 1) {
+                            let timer = std::time::Instant::now();
+                            x();
+                            println!("Took {:?}", timer.elapsed());
+                        } else {
+                            println!("Not implemented");
+                        }
+                    } else {
+                        println!("Not implemented");
+                    }
+                } else {
+                    println!("Must enter numbers!");
+                }
+            } else {
+                println!("Pass day and part as commandline parameters");
+            }
+        } else {
+            println!("Must enter numbers!");
+        }
+    } else {
+        println!("Pass day and part as commandline parameters");
+    }
+}
